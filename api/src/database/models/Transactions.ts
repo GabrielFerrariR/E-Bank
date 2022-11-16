d6aaf2230f1e8766aab8565c4fa659e2b66525dc
Gabriel <gabrielferrari601@gmail.com>
2022-11-16 20:04:40 -0300

    feat: cria model de transactions e suas associations

---
+++ api/src/database/models/Transactions.ts
@@ -0,0 +1,57 @@
+import {
+  Model, DataTypes,
+} from 'sequelize';
+import db from '.';
+import Accounts from './Accounts';
+
+class Transactions extends Model {
+  public id!: number;
+  public debitedAccountId!: number;
+  public creditedAccountId!: number;
+  public value!: number;
+}
+
+Transactions.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  debitedAccountId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: {
+      model: 'accounts',
+      key: 'id',
+    },
+  },
+  creditedAccountId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    onUpdate: 'CASCADE',
+    onDelete: 'CASCADE',
+    references: {
+      model: 'accounts',
+      key: 'id',
+    },
+  },
+}, {
+  sequelize: db,
+  modelName: 'Transactions',
+  timestamps: true,
+});
+
+
+Transactions.belongsTo(Accounts, {
+  foreignKey: 'debitedAccountId',
+  as: 'debitedAccount',
+});
+Transactions.belongsTo(Accounts, {
+  foreignKey: 'creditedAccountId',
+  as: 'creditedAccount',
+});
+
+export default Transactions;
