d1f708537988ce66cfd77bc62bc0ea10f74cbb84
Gabriel <gabrielferrari601@gmail.com>
2022-11-17 17:35:39 -0300

    feat: middleware derror

---
+++ api/src/middleWares/errorMiddleWare.ts
@@ -0,0 +1,28 @@
+import { ErrorRequestHandler } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { ZodError } from 'zod';
+import { ErrorTypes, errorCatalog } from '../errors/catalog';
+
+const errorHandler: ErrorRequestHandler = ( 
+  err: Error | ZodError, 
+  _req,
+  res,
+  _next,
+) => {
+  if (err instanceof ZodError) { 
+    return res.status(StatusCodes.BAD_REQUEST).json({ message: err.issues });
+  }
+
+  const messageAsErrorType = err.message as ErrorTypes;
+
+  const mappedError = errorCatalog[messageAsErrorType];
+  if (mappedError) {
+    const { httpStatus, message } = mappedError;
+    return res.status(httpStatus).json({ error: message });
+  }
+
+  console.error(err);
+  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ message: 'internal error' });
+};
+
+export default errorHandler;
