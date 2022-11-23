c1d79f2ac17c674a2af01506293f15d066ff6e6c
Gabriel <gabrielferrari601@gmail.com>
2022-11-23 17:16:06 -0300

    feat(client):

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Routes from './Router';
-import Contexts from './context';
+import { Provider } from './context/Provider';
 import './assets/reset.css';
 import './assets/colors.css';
 import './assets/typography.css';
+import './assets/global.css';
 
-// if (process.env.NODE_ENV !== 'production') {
-//   import ('react-axe').then((axe) => {
-//     axe.default(React, ReactDOM, 1000);
-//     ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-//       <React.StrictMode>
-//         <Contexts>
-//           <Routes />
-//         </Contexts>
-//       </React.StrictMode>,
-//     );
-//   });
-// }
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-);
+if (process.env.NODE_ENV !== 'production') {
+  import ('react-axe').then((axe) => {
+    axe.default(React, ReactDOM, 1000);
+    ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+      <React.StrictMode>
+        <Routes />
+      </React.StrictMode>,
+    );
+  });
+}
--- client/src/services/api.ts
+++ client/src/services/api.ts
@@ -1,9 +1,29 @@
 import axios from 'axios';
+import UserRequest from '../interfaces/UserRequest';
 
 const api = axios.create({
-  baseURL: "http://localhost:3001/"
+  baseURL: 'http://localhost:3001/',
 });
 
-const request = async () => await api.get('/');
+const request = async () => api.get('/');
+
+export const setToken = async (token: string) => {
+  api.defaults.headers.common.Authorization = token;
+};
+
+export const requestLogin = async (param: string, body: UserRequest) => {
+  const { data } = await api.post(`${param}`, body);
+  return data;
+};
+
+export const validateLogin = async () => {
+  const { data } = await api.post('/login/validate');
+  return data;
+};
+
+export const requestData = async (param: string) => {
+  const { data } = await api.get(`${param}`);
+  return data;
+};
 
 export default request;
