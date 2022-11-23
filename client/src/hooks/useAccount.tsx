e26e5c77e3d62bdc28025158cd878f8d39759fe8
Gabriel <gabrielferrari601@gmail.com>
2022-11-23 19:49:11 -0300

    feat: custom hook de resgatar informaçoes da conta

---
+++ client/src/hooks/useAccount.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+import IAccount from '../interfaces/IAccount';
+import { requestData, setToken } from '../services/api';
+
+function useAccount() {
+  const [account, setAccount] = useState<IAccount>({
+    id: 0,
+    username: '',
+    accountId: 0,
+    account: {
+      id: 0,
+      balance: '00.00',
+    },
+  });
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) setToken(token);
+    const fetch = async () => {
+      try {
+        const data = await requestData('/account/balance');
+        setAccount(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetch();
+  }, []);
+  return account;
+}
+
+export default useAccount;
