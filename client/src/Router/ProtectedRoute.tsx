a903dbc8851265a3d2bd7bac49f7b5fa181616d8
Gabriel <gabrielferrari601@gmail.com>
2022-11-25 17:07:14 -0300

    fix(client): corrige erro de rotas protegidas

--- client/src/Router/ProtectedRoute.tsx
+++ client/src/Router/ProtectedRoute.tsx
@@ -1,19 +1,18 @@
+import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
-// import { useToken, useToken } from '../context/Provider';
 import { IChildren } from '../interfaces/IChildren';
 import { validateLogin, setToken } from '../services/api';
 
 function ProtectedRoute({ children }: IChildren) {
+  const [isValid, setIsValid] = useState(true);
   const token = localStorage.getItem('token');
   if (!token) return <Navigate to="/login" />;
-  try {
-    setToken(token);
-    validateLogin();
-    // eslint-disable-next-line react/jsx-no-useless-fragment
-    return (<>{ children }</>);
-  } catch (error) {
-    return <Navigate to="/login" />;
-  }
+  setToken(token);
+  validateLogin().catch(() => {
+    setIsValid(false);
+  });
+  // eslint-disable-next-line react/jsx-no-useless-fragment
+  return isValid ? (<>{ children }</>) : <Navigate to="/login" />;
 }
 
 export default ProtectedRoute;
