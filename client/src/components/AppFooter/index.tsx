dbe89412e95d339f20991f5c66a463a88e6c22a5
Gabriel <gabrielferrari601@gmail.com>
2022-11-26 12:32:33 -0300

    feat(client): adiciona o footer para telas pequenas

---
+++ client/src/components/AppFooter/index.tsx
@@ -0,0 +1,26 @@
+import WalletIcon from '@mui/icons-material/Wallet';
+import LogoutIcon from '@mui/icons-material/Logout';
+import ManageSearch from '@mui/icons-material/ManageSearch';
+import { useNavigate } from 'react-router-dom';
+import style from './style.module.css';
+import moneyTransferIcon from '../../assets/images/moneyTransfer.svg';
+
+function AppFooter() {
+  const navigate = useNavigate();
+  return (
+    <footer className={style.footer}>
+      <WalletIcon sx={{ color: 'white' }} fontSize="large" onClick={() => navigate('/balance')} />
+      <button
+        type="button"
+        onClick={() => navigate('/transaction')}
+        className={style.button}
+      >
+        <img src={moneyTransferIcon} alt="money transfer" />
+      </button>
+      <ManageSearch sx={{ color: 'white' }} fontSize="large" onClick={() => navigate('/history')} />
+      <LogoutIcon sx={{ color: 'white' }} fontSize="large" />
+    </footer>
+  );
+}
+
+export default AppFooter;
