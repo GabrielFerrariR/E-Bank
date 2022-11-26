806449470bd92c08e6faf9359294340ea9284de9
Gabriel <gabrielferrari601@gmail.com>
2022-11-26 15:47:45 -0300

    style: melhora a estilização dos cards de transaçoes

--- client/src/components/TransactionCard./index.tsx
+++ client/src/components/TransactionCard./index.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from 'react';
 import ITransactions from '../../interfaces/ITransactions';
-import { requestData } from '../../services/api';
+import cashinIcon from '../../assets/images/cashIn.svg';
+import cashoutIcon from '../../assets/images/cashOut.svg';
 import style from './style.module.css';
 
 function TransactionCard({
@@ -17,10 +17,13 @@ function TransactionCard({
   const createdDate = new Date(createdAt);
 
   return (
-    <section>
-      <h3>{isCashOut ? CreditedUsername : debitedAccountUserName }</h3>
-      <h2 className={isCashOut ? style.cashOut : style.cashIn}>{Number(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h2>
-      <p>{createdDate.toLocaleString()}</p>
+    <section className={style.card}>
+      <img src={isCashOut ? cashoutIcon : cashinIcon} alt="tranfer-type" />
+      <div className={style.card_header}>
+        <h4>{isCashOut ? CreditedUsername : debitedAccountUserName }</h4>
+        <p className="label-sm">{createdDate.toLocaleDateString('pt-BR', { month: 'short', day: '2-digit' })}</p>
+      </div>
+      <h3 className={isCashOut ? style.cashOut : style.cashIn}>{Number(value).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}</h3>
     </section>
   );
 }
